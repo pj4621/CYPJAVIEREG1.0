af55c7773e6361f675f0dd9c1f8f3245d1e3da57
pj4621 <tsp4621@gmail.com>
2019-09-18 22:22:45 -0500

    Tarea

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,8 @@
+Base = int(input("Dame el valor de la Base:"))
+Altura = int(input("Dame el valor de la Altura:"))  
+Area = Base * Altura
+SLados = Base + Altura
+Perimetro = SLados * 2
+print(f"El valor del Area es { Base * Altura }")
+print(f"El valor del Perimetro es { SLados * 2 }")
+
---
+++ libro/ejemplo1_11.py
@@ -0,0 +1,6 @@
+Leer = ("A,B,C,D")
+Escribir = ("D,C,B,A")
+
+print(Leer)
+print(Escribir)
+
